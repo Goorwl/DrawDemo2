eab5643b4ee155e511ea6dcee7dfbdc96913fb50
goorwl <goorwl@163.com>
2019-07-11 12:18:37 +0800

    交作业

---
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice07ColorMatrixColorFilterView.java
@@ -0,0 +1,52 @@
+package com.hencoder.hencoderpracticedraw2.practice;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.hencoder.hencoderpracticedraw2.R;
+
+public class Practice07ColorMatrixColorFilterView extends View {
+    Paint  paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Bitmap bitmap;
+    private ColorMatrixColorFilter mColorFilter;
+
+    public Practice07ColorMatrixColorFilterView(Context context) {
+        super(context);
+    }
+
+    public Practice07ColorMatrixColorFilterView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice07ColorMatrixColorFilterView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    {
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.batman);
+
+        // 使用 setColorFilter() 设置一个 ColorMatrixColorFilter
+        // 用 ColorMatrixColorFilter.setSaturation() 把饱和度去掉
+        mColorFilter = new ColorMatrixColorFilter(new float[]{
+                1, 0, 0, 0, 0,
+                0, 1, 0, 0, 0,
+                0, 0, 1, 0, 0,
+                0, 0, 0, 1, 0
+        });
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        paint.setColorFilter(mColorFilter);
+        canvas.drawBitmap(bitmap, 0, 0, paint);
+    }
+}
